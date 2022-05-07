0af6f89173ac2cbe29eab43eb80a19bd01589ed4
luoyuzhao <290330505@qq.com>
2022-05-07 13:47:31 +0800

    Update main.py

--- tools/main.py
+++ tools/main.py
@@ -342,11 +342,11 @@ class SdkSyncPackages:
 
 def main():
     init_logger()
-    with open('/rt-thread/sdk-index/tools/bsp_update_url.json', "r") as f:
+    with open('temp_update_pkgs.json', "r") as f:
         text = json.loads(f.read())
     print(text)
     with open("temp_update_pkgs.json", "a") as f:
-        f.write(str(json.dumps("['123','456','789','123','456','789','123','456','789','123','456','789','123','456','789','123','456','789','123','456','789','123','456','789','123','456','789','123','456','789','123','456','789','123','456','789']")))
+        f.write(str(json.dumps("['123','456','789']")))
     logging.info("#####################")
 
 if __name__ == "__main__":
